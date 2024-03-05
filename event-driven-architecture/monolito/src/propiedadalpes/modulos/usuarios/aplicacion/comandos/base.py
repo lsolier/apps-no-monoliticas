0a58236d0ceb174838a13d3c05c6ff9fff14da70
Dave-Uniandes <d.borrerov@uniandes.edu.co>
2024-03-04 20:15:34 -0500

    adding user structure

--- event-driven-architecture/monolito/src/propiedadalpes/modulos/usuarios/aplicacion/comandos/base.py
+++ event-driven-architecture/monolito/src/propiedadalpes/modulos/usuarios/aplicacion/comandos/base.py
@@ -1,2 +1,16 @@
 from propiedadalpes.seedwork.aplicacion.comandos import ComandoHandler
 from propiedadalpes.modulos.usuarios.infraestructura.fabricas import FabricaRepositorio
+from propiedadalpes.modulos.usuarios.dominio.fabricas import FabricaUsuarios
+
+class IngestarUsuarioBaseHandler(ComandoHandler):
+    def __init__(self):
+        self._fabrica_repositorio: FabricaRepositorio = FabricaRepositorio()
+        self._fabrica_usuarios: FabricaUsuarios = FabricaUsuarios()
+
+    @property
+    def fabrica_repositorio(self):
+        return self._fabrica_repositorio
+    
+    @property
+    def _fabrica_usuarios(self):
+        return self._fabrica_usuarios
